505c40bbe1f1f7b2cae63582aea92b08b78ae0d1
zepengtan <113353950+zepengtan@users.noreply.github.com>
2022-09-23 09:31:09 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,4 @@
+var vg_1 = "map.vg.json";
+vegaEmbed("#map", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
